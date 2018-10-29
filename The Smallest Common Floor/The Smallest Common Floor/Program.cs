edfa89b0696c25668640334b0189cda16a914b58
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-29 20:51:22 +0300

    Add files via upload

---
+++ The Smallest Common Floor/The Smallest Common Floor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Smallest_Common_Floor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Kullanıcının vermiş olduğu 2 sayının en küçük ortak katını(ekok) bulabilecek algoritmayı tasarlayınız.
+
+            int sayı_1 = 0, sayı_2 = 0;
+            Console.Write("1. sayıyı giriniz : ");
+            sayı_1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. sayıyı giriniz : ");
+            sayı_2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayı_1 % sayı_2 == 0)
+            {
+                Console.WriteLine("Girmiş olduğunuz sayıların en küçük ortak katı = " + sayı_1);
+
+            }
+            else if (sayı_2 % sayı_1 == 0)
+            {
+                Console.WriteLine("Girmiş olduğunuz sayıların en küçük ortak katı = " + sayı_2);
+            }
+            else
+            {
+                Console.WriteLine("Girmiş olduğunuz sayıların en küçük ortak katı = " + (sayı_1 * sayı_2));
+            }
+
+
+
+        }
+    }
+}
---
+++ The Smallest Common Floor/The Smallest Common Floor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("The Smallest Common Floor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("The Smallest Common Floor")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("25b9324d-17e3-461b-a35f-bf7fbebd7cf5")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
